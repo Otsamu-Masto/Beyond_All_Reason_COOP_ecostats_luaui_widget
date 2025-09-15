a279cf0994a49dfe7dddb474ed83468c1cdf6a6b
Otsamu-Masto <masto1catisbetter@gmail.com>
2025-09-15 13:44:48 +0800

    Create coop_ecostat.lua

---
+++ coop_ecostat.lua
@@ -0,0 +1,14 @@
+function widget:GetInfo()
+    return {
+        name = "coop_ecostat (Otsamu_Masto)",
+        desc = [[
+idk bruh
+]],
+        author = "Otsamu_Masto",
+        version = "2.7.5",
+        date = "2025",
+        license = "GNU GPL, v2 or later",
+        layer = -999,
+        enabled = true
+    }
+end
